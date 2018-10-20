1468488fb30799d285cb78a8ff8f99586dfab469
e <269219362ljf@gmail.com>
2018-10-20 21:32:35 +0800

    change position

--- askr-server/src/main/java/askr/yaggdrasills/util/AskrServerApplication.java
+++ askr-server/src/main/java/askr/yaggdrasills/util/AskrServerApplication.java
@@ -1,4 +1,4 @@
-package askryggdrasillsapi.askrserver;
+package askr.yaggdrasills.util;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
