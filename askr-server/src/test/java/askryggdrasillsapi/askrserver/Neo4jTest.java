ba15437a193567b2b57fc553324f95af772554e6
jianfeng.lu <269219362@qq.com>
2018-12-05 23:32:51 +0800

    add neo4j

---
+++ askr-server/src/test/java/askryggdrasillsapi/askrserver/Neo4jTest.java
@@ -0,0 +1,38 @@
+package askryggdrasillsapi.askrserver;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Neo4jTest extends AskrServerApplicationTests {
+
+    Logger logger = LoggerFactory.getLogger(Neo4jTest.class);
+
+//    @Autowired
+//    UserRepository userRepository;
+//
+//    @Test
+//    public void createUserNode(){
+//        System.out.println(userRepository);
+//        UserNode userNode = new UserNode();
+//        userNode.setName("老鼠");
+//        userNode.setUserId("123");
+//        UserNode save = userRepository.save(userNode);
+//        logger.info(save.toString());
+//        Assert.assertTrue(save!=null);
+//    }
+//
+//    @Test
+//    public void delAll(){
+//        userRepository.deleteAll();
+//    }
+
+
+
+
+
+
+
+
+}
